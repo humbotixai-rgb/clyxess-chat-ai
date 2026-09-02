6894c05173c8cbbbeb9e705c09c1628b6b013866
humbotixai-rgb <humbotixai@gmail.com>
2026-09-02 15:46:05 +0530

    Update clyxesschat.py

--- clyxesschat.py
+++ clyxesschat.py
@@ -1729,7 +1729,7 @@ if prompt:
         for word in response.split(" "):
             typed += word + " "
             placeholder.markdown(typed + "▌")
-            time.sleep(0.09)
+            time.sleep(0.02)
         placeholder.markdown(response)
         if sources: st.caption("Sources:\n"+sources)
         st.caption(" ClyxessChat AI  | Secure • Fast • Private")
